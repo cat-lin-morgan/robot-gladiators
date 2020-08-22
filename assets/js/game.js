096e0c40a7f5e1cc55d32db5208882510604f47c
cat - lin <plummorgan@gmail.com>
2020-08-21 17:35:18 -0700

    updated shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -178,17 +178,15 @@ var shop = function() {
     //ask the player wehat they'sd like to do
     var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
     //sue switch to carry out the action
+    shopOptionPrompt = parseInt(shopOptionPrompt)
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             // do nothing, so function will end
             break;
